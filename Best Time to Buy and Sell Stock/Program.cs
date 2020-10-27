abe1b3dd2e0f9ae5e9af2a51b8d147e9e93a4e87
Jaydeep Shil <ED8A@eurofins.com>
2020-10-27 16:03:18 +0530

    Add project files.

---
+++ Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Best_Time_to_Buy_and_Sell_Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            int min = int.MaxValue;
+            for(int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < min) min = prices[i];
+                else if (prices[i] - min > maxProfit) maxProfit = prices[i] - min;
+            }
+            return maxProfit;
+        }
+    }
+
+}
